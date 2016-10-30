04cc5172b3c84042be5b8da986b8048fe01500bb
rmichal2 <rmichal2@illinois.edu>
2016-10-30 14:48:51 -0500

    starting some functions for folding not done yet. also adding comments

--- js/animation.js
+++ js/animation.js
@@ -7,6 +7,8 @@ function performFold(){
       foldingLine.visible=false; 
         
       var test = uGrid.retrieveF(foldingLine.geometry.vertices);
+      //Object3D.rotateOnAxis( axis, angle );
+
       //performfold on all mesh, visualFoldLines, and boarderLines.
       //boarderLines and mesh need to be rotated 180deg around the foldingLine.
       //visualFoldLines need to have the end point moved to the interesection of the foldingLine and the visualFoldLine if they cross. might be tricky to find witch point is the end point. it is the point clossest to the foldingLine.
