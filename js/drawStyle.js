194314cf46cedb5d1741016ca85da949aab3194b
= <=>
2016-11-04 11:47:08 -0500

    change some rad to checkboxes

--- js/drawStyle.js
+++ js/drawStyle.js
@@ -3,39 +3,35 @@ function checkDrawStyle(){
     scene.traverse( function(child){
       if( child instanceof THREE.Mesh && child.name=="mesh")
         child.material.wireframe= false;
-      else if(child instanceof THREE.Line && child.name=="visualFoldLine")
-          child.visible=false;
-      else if(child instanceof THREE.Mesh && child.name=="borders")
-        child.visible=false;
-    });
-  }
-  else if (document.getElementById('borders').checked) {
-     scene.traverse( function(child){
-      if( child instanceof THREE.Mesh && child.name=="mesh")
-        child.material.wireframe= false;
-      else if(child instanceof THREE.Line && child.name=="visualFoldLine")
-        child.visible=false;
-      else if(child instanceof THREE.Mesh && child.name=="borders")
-        child.visible=true;
     });
   }
   else if (document.getElementById('wireframe').checked) {
     scene.traverse( function(child){
       if(child instanceof THREE.Mesh && child.name=="mesh")
         child.material.wireframe= true;
-      else if(child instanceof THREE.Line && child.name=="visualFoldLine")
-          child.visible=false;
-      else if(child instanceof THREE.Mesh && child.name=="borders")
-        child.visible=false;
     });
   }
-  else if (document.getElementById('foldLines').checked) {
+  if (document.getElementById('borders').checked) {
      scene.traverse( function(child){
-      if( child instanceof THREE.Mesh && child.name=="mesh")
-        child.material.wireframe= false;
-      else if(child instanceof THREE.Line && child.name=="visualFoldLine")
+       if(child instanceof THREE.Mesh && child.name=="borders")
+         child.visible=true;
+    });
+  }
+  else if (!document.getElementById('borders').checked) {
+     scene.traverse( function(child){
+       if(child instanceof THREE.Mesh && child.name=="borders")
+         child.visible=false;
+    });
+  }
+  if (document.getElementById('foldLines').checked) {
+     scene.traverse( function(child){
+      if(child instanceof THREE.Line && child.name=="visualFoldLine")
         child.visible=true;
-      else if(child instanceof THREE.Mesh && child.name=="borders")
+    });
+  }
+  else if (!document.getElementById('foldLines').checked) {
+     scene.traverse( function(child){
+      if(child instanceof THREE.Line && child.name=="visualFoldLine")
         child.visible=false;
     });
   }
